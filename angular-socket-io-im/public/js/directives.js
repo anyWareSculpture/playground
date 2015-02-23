fa75c3e8a67b4c2c36c28e4db9567c7c37b0196e
jrjamies <jrjamies@uwaterloo.ca>
2015-02-22 19:09:24 -0500

    Move sculpture state to custom directive

--- angular-socket-io-im/public/js/directives.js
+++ angular-socket-io-im/public/js/directives.js
@@ -6,6 +6,12 @@
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
-      elm.text(version);
+        elm.text(version);
     };
-  }]);
+  }]).
+
+  directive('sculptureState', function() {
+    return {
+        templateUrl: 'partials/sculptureState.jade'
+    };
+  });
